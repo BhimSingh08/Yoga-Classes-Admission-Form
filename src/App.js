8bd29c62427bac8f6f179343d79eb3b33ab6d0db
Bhim Singh <108855385+BhimSingh08@users.noreply.github.com>
2023-12-17 20:22:21 +0530

    Yoga-Classes-Registration-form-Assignmen-Flexmoney

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Form from './components/Form';
+
+// Define functional component App
+function App() {
+ // Render the Form component within the App component
+ return (
+    <div className="app">
+      <Form></Form>
+    </div>
+ );
+}
+
+// Export the App component
+export default App;
